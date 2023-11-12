1b76c9f7564889bb69555a3e95a63a9ef30c8171
aliuazeez <aliuazeez6m@gmail.com>
2023-11-12 14:14:40 +0000

    printf0

--- _printf.c
+++ _printf.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _printf - function that produces output according to format
  * @format: character string
- *
  * Return: number of characters printed or 0
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed = 0;
 	char c;
 	char *str;
-	int len = 0;
 
 	va_start(args, format);
 	if (*format == '\0')
-	{
 		return (0);
-	}
 	while (*format != '\0')
 	{
 		if (*format != '%')
@@ -40,12 +34,12 @@ int _printf(const char *format, ...)
 			else if (*format == 's')
 			{
 				str = va_arg(args, char *);
-				while (str[len] != '\0')
+				while (*str)
 				{
-					len++;
+					write(1, str, 1);
+					str++;
+					printed++;
 				}
-				write(1, str, len);
-				printed = printed + len;
 			}
 			else if (*format == '%')
 			{
@@ -58,4 +52,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (printed);
 }
-
