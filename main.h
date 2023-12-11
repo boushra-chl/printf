b180c7442947242b5675d787883b1370eb281f56
boushra-chl <bchilouch1998@gmail.com>
2023-12-11 16:00:09 +0000

    update

--- main.h
+++ main.h
@@ -16,6 +16,5 @@ int decimal_to_hexadecimal(unsigned int num, int indice);
 void _print_hex(int d);
 void _print_HEX(int d);
 int print_hexadecimal(unsigned long num);
-int print_pointer(void *ptr);
 
 #endif
